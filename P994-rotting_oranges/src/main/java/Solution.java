71d7a7e1cf4da07fa84ab928ddc72acbfada031a
Chrissy <1804659599@qq.com>
2024-05-23 11:05:31 +0800

    搭建P994环境

---
+++ P994-rotting_oranges/src/main/java/Solution.java
@@ -0,0 +1,24 @@
+/*
+ * @Author: Chrissy 1804659599@qq.com
+ * @Date: 2024-05-23 10:56:56
+ * @LastEditors: Chrissy 1804659599@qq.com
+ * @LastEditTime: 2024-05-23 11:04:57
+ * @Description: to be added
+ */
+public class Solution {
+    public int orangesRotting(int[][] grid){
+        int col = grid.length;
+        int row = gird[0].length;
+        int times = 0;
+        for (int i = 0; i < col; i++){
+            for (int j = 0; j < row; j++){
+                
+            }
+        }
+        return times;
+    }
+
+    private void oneDFS(int[]grid, int col, int row){
+
+    }
+}
