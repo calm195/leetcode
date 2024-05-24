47724464df8b771b4448d882e47ffa2512366c64
Chrissy <1804659599@qq.com>
2024-05-24 15:47:08 +0800

    an error of P 207 maybe

---
+++ P207-course_schedule/src/main/java/Solution.java
@@ -0,0 +1,12 @@
+/*
+ * @Author: Chrissy 1804659599@qq.com
+ * @Date: 2024-05-24 15:00:04
+ * @LastEditors: Chrissy 1804659599@qq.com
+ * @LastEditTime: 2024-05-24 15:00:05
+ * @Description: to be added
+ */
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+    }
+}
