ff90e4c636b20961983d73d4f88cf7d9213ac40c
wx128 <wx2048@protonmail.com>
2024-05-25 22:47:53 +0800

    完成P054螺旋矩阵

--- P054-spiral_matrix/src/main/java/SpiralMatrix.java
+++ P054-spiral_matrix/src/main/java/SpiralMatrix.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,10 +7,10 @@ import java.util.List;
  */
 public class SpiralMatrix {
     public static void main(String[] args) {
-//        int[][] matrix = {{1, 2, 3}, {5, 6, 7}, {9, 10, 11}};
+        int[][] matrix = {{1, 2, 3}, {5, 6, 7}, {9, 10, 11}};
 //        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-//        int[][] matrix = {{1, 2, 3}, {5, 6, 7}, {9, 10, 11}, {13, 14, 15}, {17, 18, 19}};
-        int[][] matrix = {{1, 2, 3, 4, 5, 6, 7}, {9, 10, 11, 12, 13, 14, 15}, {16, 17, 18, 19, 20, 21, 22}};
+        //int[][] matrix = {{1, 2, 3}, {5, 6, 7}, {9, 10, 11}, {13, 14, 15}, {17, 18, 19}};
+        //int[][] matrix = {{1, 2, 3, 4, 5, 6, 7}, {9, 10, 11, 12, 13, 14, 15}, {16, 17, 18, 19, 20, 21, 22}};
         SpiralMatrix test = new SpiralMatrix();
 
         System.out.println(test.spiralOrder(matrix));
@@ -27,45 +26,57 @@ public class SpiralMatrix {
         int bottom_col = 0;
 
         int top_j = 0;
-        int top_i = 0;
+        int top_i = 0; 
         int bottom_j = n - 1;
         int bottom_i = m - 1;
 
         List<Integer> list = new ArrayList<>();
 
-        while (top_row <= bottom_row) {
+        while (top_row <= bottom_row && bottom_col <= top_col) {
+            if (top_row == bottom_row && bottom_col == top_col) {
+                list.add(matrix[top_row][top_col]);
+                break;
+            }
             while (top_j < top_col) {
                 list.add(matrix[top_row][top_j]);
                 top_j++;
             }
             top_j = bottom_col + 1;
+            System.out.println(list);
             while (top_i < bottom_row) {
                 list.add(matrix[top_i][top_col]);
                 top_i++;
             }
+            System.out.println(list);
             top_i = top_row + 1;
             if (bottom_row != top_row) {
                 while (bottom_j > bottom_col) {
                     list.add(matrix[bottom_row][bottom_j]);
                     bottom_j--;
                 }
+            } else {
+                list.add(matrix[bottom_row][top_col]);
             }
             bottom_j = top_col - 1;
+            System.out.println(list);
             if (top_col != bottom_col){
                 while (bottom_i > top_row) {
                     list.add(matrix[bottom_i][bottom_col]);
                     bottom_i--;
                 }
+            } else {
+                list.add(matrix[bottom_row][bottom_col]);
             }
             bottom_i = bottom_row - 1;
 
+            System.out.println(list);
+            System.out.println("---------");
+            System.out.println(bottom_row + "  " + top_row);
+            System.out.println("---------");
             top_col--;
+            top_row++;
             bottom_row--;
             bottom_col++;
-            top_row++;
-        }
-        if (m % 2 == 1 && n % 2 == 1){
-            list.add(matrix[m / 2][n / 2]);
         }
         return list;
     }
