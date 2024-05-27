f6a6d411f89768e2af030517680d02e5439f49c7
Chrissy <1804659599@qq.com>
2024-05-27 21:17:13 +0800

    P169 Boyer-Moore算法 completed

---
+++ P169-majority_element/src/main/java/Main.java
@@ -0,0 +1,16 @@
+/*
+ * @Author: Chrissy 1804659599@qq.com
+ * @Date: 2024-05-27 20:51:42
+ * @LastEditors: Chrissy 1804659599@qq.com
+ * @LastEditTime: 2024-05-27 21:16:02
+ * @Description: to be added
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = new int[] {
+            2,2,1,1,1,2,2,2
+        };
+        Solution solution = new Solution();
+        System.out.println(solution.majorityElement(nums));
+    }
+}
