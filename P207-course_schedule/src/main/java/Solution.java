f5c64f8ae972cb99f9881647ea5298814a636ca2
Chrissy <1804659599@qq.com>
2024-05-27 10:29:43 +0800

    尝试使用邻接表,但是意识到原始通用邻接表性能对P207效率可能不佳,于是准备更换

--- P207-course_schedule/src/main/java/Solution.java
+++ P207-course_schedule/src/main/java/Solution.java
@@ -2,7 +2,7 @@
  * @Author: Chrissy 1804659599@qq.com
  * @Date: 2024-05-24 15:00:04
  * @LastEditors: Chrissy 1804659599@qq.com
- * @LastEditTime: 2024-05-24 16:02:54
+ * @LastEditTime: 2024-05-27 10:28:06
  * @Description: to be added
  */
 
@@ -10,7 +10,43 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Solution {
+    private class VNode {
+        int value;
+        ENode toNodes;
+
+        VNode(int value){
+            this.value = value;
+        }
+
+        void addEnode(int value){
+            if (toNodes == null) {
+                toNodes = new ENode();
+                toNodes.node = new VNode(value);
+                return;
+            }
+            ENode tmp = toNodes;
+            while (tmp.next != null) {
+                tmp = tmp.next;
+            }
+            tmp.next = new ENode();
+            tmp.next.node = new VNode(value);
+        }
+    }
+
+    private class ENode{
+        VNode node;
+        ENode next;
+    }
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        VNode[] courses = new VNode[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            courses[i] = new VNode(i); 
+        } 
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            courses[prerequisites[i][0]].addEnode(prerequisites[i][1]);
+        }
         return true;
     }
 }
