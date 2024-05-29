1eea1406b1785cf4b8d3b1dd583d850c72d15ae6
wx128 <wx2048@protonmail.com>
2024-05-29 22:54:15 +0800

    none

--- P207-course_schedule/src/main/java/Solution.java
+++ P207-course_schedule/src/main/java/Solution.java
@@ -1,8 +1,8 @@
 /*
  * @Author: Chrissy 1804659599@qq.com
  * @Date: 2024-05-24 15:00:04
- * @LastEditors: Chrissy 1804659599@qq.com
- * @LastEditTime: 2024-05-24 16:02:54
+ * @LastEditors: wx128 wx2048@protonmail.com
+ * @LastEditTime: 2024-05-25 22:50:51
  * @Description: to be added
  */
 
@@ -11,6 +11,7 @@ import java.util.HashSet;
 
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
         return true;
     }
 }
