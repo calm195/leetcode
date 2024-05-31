976ca883bee55462d7fa8dd1af82546a49376a61
Chrissy <1804659599@qq.com>
2024-05-31 09:10:17 +0800

    prepare P075 actually

--- P763-partition_labels/src/main/java/Solution.java
+++ P763-partition_labels/src/main/java/Solution.java
@@ -1,5 +1,12 @@
+/*
+ * @Author: Chrissy 1804659599@qq.com
+ * @Date: 2024-05-31 09:07:59
+ * @LastEditors: Chrissy 1804659599@qq.com
+ * @LastEditTime: 2024-05-31 09:09:21
+ * @Description: to be added
+ */
 public class Solution {
-    public List<Integer> partitionLabels(String s) {
+    public List<Integer> partitionLabels(String s){
 
     }
 }
