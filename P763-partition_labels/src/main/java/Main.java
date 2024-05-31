23e97dc7f7cfad9a8866723a4faa4b3e80855c42
Chrissy <1804659599@qq.com>
2024-05-31 16:48:08 +0800

    no idea with P763

--- P763-partition_labels/src/main/java/Main.java
+++ P763-partition_labels/src/main/java/Main.java
@@ -1,5 +1,12 @@
+/*
+ * @Author: Chrissy 1804659599@qq.com
+ * @Date: 2024-05-31 09:04:54
+ * @LastEditors: Chrissy 1804659599@qq.com
+ * @LastEditTime: 2024-05-31 16:33:15
+ * @Description: to be added
+ */
 public class Main {
-    public static void(String[] args){
+    public static void main(String[] args){
         Solution solution = new Solution();
         String s = "ababcbacadefegdehijhklij";
         System.out.println(solution.partitionLabels(s));
--- P763-partition_labels/src/main/java/Solution.java
+++ P763-partition_labels/src/main/java/Solution.java
@@ -2,11 +2,21 @@
  * @Author: Chrissy 1804659599@qq.com
  * @Date: 2024-05-31 09:07:59
  * @LastEditors: Chrissy 1804659599@qq.com
- * @LastEditTime: 2024-05-31 09:09:21
+ * @LastEditTime: 2024-05-31 16:36:25
  * @Description: to be added
  */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public List<Integer> partitionLabels(String s){
+        int len = s.length();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            
+        }
 
+        return list;
     }
 }
