dfda77cdd39077ca970364d66ee5d7b26873e8d8
chrissy <wx2178@126.com>
2024-09-27 21:54:08 +0800

    add desc of P2516

--- P2516-take_k_chars/src/main/java/Main.java
+++ P2516-take_k_chars/src/main/java/Main.java
@@ -1,3 +1,21 @@
+/**
+ * You are given a string s consisting of the characters 'a', 'b', and 'c' and a non-negative integer k. 
+ * Each minute, you may take either the leftmost character of s, or the rightmost character of s.
+ * Return the minimum number of minutes needed for you to take at least k of each character, or return -1 
+ * if it is not possible to take k of each character.
+ * 
+ * Input: s = "aabaaaacaabc", k = 2
+ * Output: 8
+ * Explanation: 
+ * Take three characters from the left of s. You now have two 'a' characters, and one 'b' character.
+ * Take five characters from the right of s. You now have four 'a' characters, two 'b' characters, and two 'c' characters.
+ *  A total of 3 + 5 = 8 minutes is needed.
+ * It can be proven that 8 is the minimum number of minutes needed.
+ * 
+ *  Input: s = "a", k = 1
+ * Output: -1
+ * Explanation: It is not possible to take one 'b' or 'c' so return -1.
+ */
 public class Main {
     public static void main(String[] args) {
         String test = "aabaaaacaabc";
