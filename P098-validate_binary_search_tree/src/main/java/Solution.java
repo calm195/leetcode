f1aa13d6da3cb4750fd47a9c6a3df368ed5911d0
chrissy <wx2178@126.com>
2024-09-30 10:16:38 +0800

    P098 递归；二叉树的中序遍历

--- P098-validate_binary_search_tree/src/main/java/Solution.java
+++ P098-validate_binary_search_tree/src/main/java/Solution.java
@@ -1,33 +1,53 @@
+
+import java.util.Deque;
+import java.util.LinkedList;
+
 /*
  * @Author: Chrissy 1804659599@qq.com
  * @Date: 2024-06-03 11:22:01
- * @LastEditors: Chrissy 1804659599@qq.com
- * @LastEditTime: 2024-06-03 11:50:39
- * @Description: to be added
+ * @LastEditors: chrissy wx2178@126.com
+ * @LastEditTime: 2024-09-30 10:15:54
+ * @Description: 
+ * 
+ * 暴力递归 
+ * 中序遍历
  */
 
 public class Solution {
     public boolean isValidBST(TreeNode root){
-        if (root == null) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean isValidBST(TreeNode node, long min, long max) {
+        if (node == null) {
             return true;
         }
-        boolean left = true, right = true;
-        if (root.left != null) {
-            if (root.left.val < root.val) {
-                left = isValidBST(root.left);
-            }
-            else {
-                left = false;
-            }
+
+        if (node.val <= min || node.val >= max){
+            return false;
         }
-        if (root.right != null) {
-            if (root.right.val > root.val) {
-                right = isValidBST(root.right);
-            }
-            else {
-                right = false;
-            }
-        }
-        return left && right;
+
+        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
     }
+
+    public boolean inorder(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        long pre = Long.MIN_VALUE;
+
+        while (!stack.isEmpty() || root != null){
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            // 总是弹出中序节点
+            root = stack.pop();
+            if (root.val <= pre) {
+                return false;
+            }
+
+            pre = root.val;
+            root = root.right;
+        }
+        return true;
+    } 
 }
