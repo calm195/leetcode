d14804359398d9bad8b0d995876895c93f73c923
chrissy <wx2048@protonmail.com>
2025-03-03 00:36:32 +0800

    P160 数学&集合

---
+++ P160-intersection_two_linkedlist/src/main/java/org/example/ListNode.java
@@ -0,0 +1,18 @@
+package org.example;
+/*
+ * @Author: chrissy wx2048@protonmail.com
+ * @Date: 2025-03-02 21:51:14
+ * @LastEditors: chrissy wx2048@protonmail.com
+ * @LastEditTime: 2025-03-02 21:51:16
+ * @Description: to be added
+ */
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
