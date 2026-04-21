d2531072eca6feef5f52bafafa05ad7a3c50750b
kurous <wx2178@126.com>
2026-04-21 16:04:16 +0800

    746 - 使用最小花费爬楼梯 - 转移方程 - 常量空间状态传递

---
+++ src/leetcode/editor/cn/T000746_MinCostClimbingStairs.java
@@ -0,0 +1,76 @@
+/**
+给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。 
+
+ 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。 
+
+ 请你计算并返回达到楼梯顶部的最低花费。 
+
+ 
+
+ 示例 1： 
+
+ 
+输入：cost = [10,15,20]
+输出：15
+解释：你将从下标为 1 的台阶开始。
+- 支付 15 ，向上爬两个台阶，到达楼梯顶部。
+总花费为 15 。
+ 
+
+ 示例 2： 
+
+ 
+输入：cost = [1,100,1,1,1,100,1,1,100,1]
+输出：6
+解释：你将从下标为 0 的台阶开始。
+- 支付 1 ，向上爬两个台阶，到达下标为 2 的台阶。
+- 支付 1 ，向上爬两个台阶，到达下标为 4 的台阶。
+- 支付 1 ，向上爬两个台阶，到达下标为 6 的台阶。
+- 支付 1 ，向上爬一个台阶，到达下标为 7 的台阶。
+- 支付 1 ，向上爬两个台阶，到达下标为 9 的台阶。
+- 支付 1 ，向上爬一个台阶，到达楼梯顶部。
+总花费为 6 。
+ 
+
+ 
+
+ 提示： 
+
+ 
+ 2 <= cost.length <= 1000 
+ 0 <= cost[i] <= 999 
+ 
+
+ Related Topics 数组 动态规划 
+ 👍 1783 👎 0
+
+*/
+
+package leetcode.editor.cn;
+// 746 - 使用最小花费爬楼梯
+public class T000746_MinCostClimbingStairs{
+    public static void main(String[] args) {
+        Solution solution = new T000746_MinCostClimbingStairs().new Solution();
+        int[] costs = {1,100,1,1,1,100,1,1,100,1};
+        System.out.println(solution.minCostClimbingStairs(costs));
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int a;
+        int b = cost[0];
+        int r = 0;
+
+        for (int i = 1; i < cost.length; i++) {
+            a = b;
+            b = r + cost[i];
+            r = Math.min(a, b);
+        }
+
+        return r;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
